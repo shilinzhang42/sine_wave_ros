7f8420a153842ac3259b4f83b836e1318de72173
shilin <zhangshilin2021@outlook.com>
2025-02-26 08:48:31 +0000

    improve plot script

--- plot/plot.py
+++ plot/plot.py
@@ -2,14 +2,14 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-# 确保文件路径正确（使用绝对路径）
+# path to the csv file
 file_path = "/home/ws/src/sine_wave_ros/sine_wave_logs/sine_wave_data.csv"
 
-# 读取 CSV 文件
+# read the csv file
 try:
     df = pd.read_csv(file_path)
 
-    # 绘制正弦波
+    # plot the sine wave
     plt.figure(figsize=(10, 5))
     plt.plot(df["Time (s)"], df["Sine Value"], label="Sine Wave", color="b")
     plt.xlabel("Time (s)")
@@ -24,6 +24,7 @@ try:
         
     plt.draw()
     plt.savefig(output_file_path)
+    plt.show()
     plt.close()
 
 except FileNotFoundError:
